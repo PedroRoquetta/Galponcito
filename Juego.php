5d107f10c4d7590d7c67fc849a445f44668cfc97
PedroRoquetta <PedroRoquetta@gmail.com>
2020-12-28 17:41:27 -0300

    inicia el proyecto

--- Juego.php
+++ Juego.php
@@ -2,6 +2,22 @@
 
 //la idea es hacer un pequeño juego de combate por turnos
 
-$player1;
-$player2;
 
+function menu (){
+    echo "-------\n";
+    echo " 1- para atacar";
+    echo " 2- para defender";
+    echo " 3- para curar";
+
+}
+
+
+$opcion=trim(fgets(STDIN));
+switch($opcion){
+case 1 : 
+        echo "juega P1";
+        $player1=trim(fgets(STDIN));
+case 2 :
+        echo "juega P2";    
+        $player2=trim(fgets(STDIN));
+}
